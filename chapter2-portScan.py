761cb639fa4cbfaac63bbca38a2e1155cecff8e1
liuxun931 <liuxun931@163.com>
2023-04-20 13:05:32 +0000

    Update chapter2-portScan.py

--- chapter2-portScan.py
+++ chapter2-portScan.py
@@ -1,42 +1,49 @@
 import optparse
 import socket
 import threading
-import os
+import os, subprocess
+import ipaddress
 
-port_list = [22,80,257,443,4434,8080,8088,19009]
+global port_list, sbnet
+
+port_list= [22,80,257,443,4434,8080,19009]
+sbnet = ipaddress.ip_network('10.100.100.0/24')
+
 socket.setdefaulttimeout(1)
 screenLock = threading.Semaphore(value = 1)
-os.system('ulimit -n 65535')
+#os.popen('ulimit -n 5000').read()
+subprocess.Popen('#!/bin/bash ulimit -n 6000', shell=True)
+
 
 def connScan(ip, port):
-    
-    connskt = socket.socket()
-    try:
-        connskt.connect((ip, port))
-        connskt.send(b'violentPython\r\n')
-        res = connskt.recv(50)
-        screenLock.acquire()
-        hostname = socket.gethostbyaddr(ip)
-        print('port: %d is opening on host %s - %s' %(port, hostname, ip))
-        print('result: %s' %res)
-        connskt.close()
-    except: 
-        screenLock.acquire()
-        print('port: %d is not opened on host %s.' %(port, ip))
-    finally:
-        screenLock.release()
-        connskt.close()
-
+	'''probe tcp socket connection by given port and ip'''
+	connskt = socket.socket(family=socket.AddressFamily.AF_INET, type=socket.SocketKind.SOCK_STREAM, proto=6)
+	connskt.settimeout(2)
+	#screenLock.acquire()
+	try:
+		connskt.connect((ip, port))
+		connskt.send(b'Hello Python\r\n')
+		res = connskt.recv(50)
+		#hostname = socket.gethostbyaddr(ip)
+		print('port: %d is opening on host %s' %(port, ip))
+		#print('result: %s' %res)
+		connskt.close()
+	except Exception as e: 
+		#screenLock.acquire()
+		#print(e)
+		#print('port: %d is not opened on host %s.' %(port, ip))
+		#screenLock.release()
+		pass
+	finally:
+		screenLock.release()
+		connskt.close()
 
 def main():
-	port_list = [22,80,257,443,4434,8080,8088,19009]
-	ip_prefix = '10.100.100.'
-	for i in range(1, 254):
-		ip_addr = ip_prefix + str(i)
-		for p in port_list:
-			t = threading.Thread(target=connScan, args=(ip_addr,int(p)))
+	for i in sbnet:
+		for p in port_list: 
+			t = threading.Thread(target=connScan, args=(str(i),int(p)))
 			t.start()
-			#connScan(ip_addr, p)
 
 if __name__ == "__main__":
     main()
+
